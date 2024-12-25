529d6d748c8af4fdb0d7878e27ab7c3b9f2d86e1
Rifat Azad <33044977+rifsxd@users.noreply.github.com>
2024-12-25 14:08:46 +0600

    kernel: fixed setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -43,7 +43,7 @@ setup_kernelsu() {
     cd "$GKI_ROOT/KernelSU-Next"
     git stash && echo "[-] Stashed current changes."
     if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
-        git checkout susfs-next-a12-5.10 && echo "[-] Switched to susfs-next-a12-5.10 branch."
+        git checkout next-susfs-a12-5.10 && echo "[-] Switched to next-susfs-a12-5.10 branch."
     fi
     git pull && echo "[+] Repository updated."
     if [ -z "${1-}" ]; then
